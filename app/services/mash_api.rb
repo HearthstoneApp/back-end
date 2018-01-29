2b34ee54d3b1dc577620c4f02954586b790559c3
MitchGianoni <mitchgianoni@yahoo.com>
2018-01-29 02:28:16 -0500

    add cards by quality api call function

--- app/services/mash_api.rb
+++ app/services/mash_api.rb
@@ -1,12 +1,18 @@
 class MashApi
 
-	BASE_URI = "https://omgvamp-hearthstone-v1.p.mashape.com/"
+	BASE_URI = "https://omgvamp-hearthstone-v1.p.mashape.com/cards"
 
 	## MASH API CALLS
 
-	def get_all_cards(cost)
-		Rails.logger.info "pulling cards that cost #{cost}"
-		response = HTTParty.get("#{BASE_URI}cards?cost=#{cost}",
+	def cards_by_cost(cost)
+		Rails.logger.info "pulling cards that cost #{cost}."
+		response = HTTParty.get("#{BASE_URI}?cost=#{cost}",
 		headers: { "X-Mashape-Key" => "qYq2t5tNu1mshlzPhXM9ewBthRm1p1LzScsjsnVhNPTn3BYoxC" })
+	end
+
+	def cards_by_quality(quality)
+		Rails.logger.info "pulling cards of #{quality} quality."
+		response = HTTParty.get("#{BASE_URI}/qualities/Epic", 
+		 headers:{ "X-Mashape-Key" => "qYq2t5tNu1mshlzPhXM9ewBthRm1p1LzScsjsnVhNPTn3BYoxC" })
 	end
 end
