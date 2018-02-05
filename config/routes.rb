6ae8cb8228bcaed264eb3b50576355b54544fe20
MitchGianoni <mitchgianoni@yahoo.com>
2018-02-04 20:26:36 -0500

    add default site controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+	root to: 'site#index'
+
+  namespace :api do
+  	namespace :v1 do
+  		resources :cards, only: [:index, :create, :destroy, :update]
+  	end
+  end
+
   resources :cards
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
